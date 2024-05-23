c9101c0feeb554b115bc785256ae82464eaf1632
Charles Mbillo <82718900+CharlesMbillo@users.noreply.github.com>
2024-05-23 17:49:48 +0000

    initial commit

--- SaccoApp/models.py
+++ SaccoApp/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-
-class Member(models.Model):
-member_id = models.CharField(max_length=10)
+#class Member(models.Model):
+#member=member_models.CharField(max_length=10)
 
 
 
