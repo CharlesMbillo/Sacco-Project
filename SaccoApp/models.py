da26d6d43c1cc96544c172488e6151ab6af3f0a2
Charles Mbillo <82718900+CharlesMbillo@users.noreply.github.com>
2024-05-23 18:07:00 +0000

    initial commmit

--- SaccoApp/models.py
+++ SaccoApp/models.py
@@ -2,16 +2,12 @@ from django.db import models
 
 #class Member(models.Model):
 #member=member_models.CharField(max_length=10)
-
-
-
+class Member(models.Model)
+Member=models.CharField(max_length=10)
 class Transaction(models.Model): member = models.ForeignKey(Member, on_delete=models.CASCADE)
-
-    transaction_type = models.CharField(max_length=10)
-
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-
-    timestamp = models.DateTimeField(auto_now_add=True)
+transaction_type = models.CharField(max_length=10)
+amount = models.DecimalField(max_digits=10, decimal_places=2)
+timestamp = models.DateTimeField(auto_now_add=True)
 
 latest_transaction = Transaction.objects.latest('id')
 
@@ -25,7 +21,7 @@ else:
 def __str__(self):
         return f'{self.transaction_type} - {self.member_id} - {self.amount} - {self.timestamp}'
 
-from rest_framework
+
 
 
 
